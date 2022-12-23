947fb907347d26148813069d0699b42b89b868ff
Andrew Zaiko <andrewzaiko@gmail.com>
2022-12-23 12:02:57 +0300

    Заменил 'homework_28_2' на 'homework_29_2'

--- homework_29_2/urls.py
+++ homework_29_2/urls.py
@@ -1,4 +1,4 @@
-"""homework_28_2 URL Configuration
+"""homework_29_2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from advertisements import views
-from homework_28_2 import settings
+from homework_29_2 import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
