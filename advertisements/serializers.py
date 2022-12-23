2985c1497ebbc4e6daaabab781ead7ad74e630f8
Andrew Zaiko <andrewzaiko@gmail.com>
2022-12-23 15:40:57 +0300

    Создал класс-сериализатор 'AdvertisementListSerializer' и переименовал 'CategoryModelSerializer'

--- advertisements/serializers.py
+++ advertisements/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework.serializers import ModelSerializer
 
-from advertisements.models import Category
+from advertisements.models import Category, Advertisement
 
 
-class CategoryModelSerializer(ModelSerializer):
+class CategoryListSerializer(ModelSerializer):
 
     class Meta:
         model = Category
         fields = "__all__"
+
+
+class AdvertisementListSerializer(ModelSerializer):
+
+    class Meta:
+        model = Advertisement
+        fields = ["id", "name", "author_id", "price"]
