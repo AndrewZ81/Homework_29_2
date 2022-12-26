91166b6f36eb848bc2eab551561ed6b65fcc969a
Andrew Zaiko <andrewzaiko@gmail.com>
2022-12-26 13:35:08 +0300

    Создал класс-сериализатор 'UserListSerializer'

--- users/serializers.py
+++ users/serializers.py
@@ -1,16 +1,19 @@
 from rest_framework.relations import SlugRelatedField, StringRelatedField
-from rest_framework.serializers import ModelSerializer, IntegerField
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from users.models import User, Location
 
 
 class UserListSerializer(ModelSerializer):
-    total_advertisements = IntegerField()
+    total_advertisements = SerializerMethodField()
     location = StringRelatedField(many=True)
 
     class Meta:
         model = User
         exclude = ["password"]
+
+    def get_total_advertisements(self, user):
+        return user.advertisement_set.filter(is_published=True).count()
 
 
 class UserDetailViewSerializer(ModelSerializer):
